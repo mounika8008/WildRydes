6be51be24697f30c253bf799646ae4567bf8a6d8
mounika8008 <50400067+mounika8008@users.noreply.github.com>
2019-09-05 16:31:09 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_4X2rzTpWR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '31304n3ju7cp2ehjkf0a6f629g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
